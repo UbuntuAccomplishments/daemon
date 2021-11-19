4794ab4ca3ec63add40b3a8e24989af2a0f537d1
Dani Llewellyn <diddledani@ubuntu.com>
2021-11-19 03:31:40 +0000

    Defer initial rescan for 5 minutes after daemon startup

--- bin/accom_daemon.dart
+++ bin/accom_daemon.dart
@@ -11,7 +11,10 @@ void main(List<String> arguments) async {
   await client.registerObject(dbusService);
 
   await dbusService.api.reloadAccomDatabase();
-  await dbusService.api.runScripts([]);
 
+  // Hourly repeating timer to regularly check for new accomplishments
   Timer.periodic(Duration(hours: 1), (_) => dbusService.api.runScripts([]));
+
+  // Oneshot timer for initial startup rescan
+  Timer(Duration(minutes: 5), () => dbusService.api.runScripts([]));
 }
