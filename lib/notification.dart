e095bb07bddf158e8cddbf03c61602e84c4fa95c
Dani Llewellyn <diddledani@ubuntu.com>
2021-11-19 02:52:05 +0000

    Ensure that desktop notify failure doesn't crash the daemon

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,14 +1,15 @@
-import 'dart:io';
-
 import 'package:desktop_notifications/desktop_notifications.dart';
 import 'package:path/path.dart' as path;
 
 var client = NotificationsClient();
 
 Future<void> notify(String message) async {
-  await client.notify(
-    message,
-    appName: 'Ubuntu Accomplishments',
-    appIcon: path.join(path.current, 'data/ubuntu-accomplishments-system.svg'),
-  );
+  try {
+    await client.notify(
+      message,
+      appName: 'Ubuntu Accomplishments',
+      appIcon:
+          path.join(path.current, 'data/ubuntu-accomplishments-system.svg'),
+    );
+  } catch (_) {}
 }
